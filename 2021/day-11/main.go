3fc0f80a7cacd7f953e435e8961aafe76d67d873
Stanislav Khalash <stanislav.khalash@sap.com>
2021-12-11 11:47:34 +0100

    WIP: implement flashing

--- 2021/day-11/main.go
+++ 2021/day-11/main.go
@@ -18,12 +18,75 @@ func main() {
 }
 
 func run(filepath string) (int, error) {
-	_, err := loadOctopusGrid(filepath)
+	octopuses, err := loadOctopusGrid(filepath)
 	if err != nil {
 		return 0, err
 	}
 
-	return 0, nil
+	totalFlashes := 0
+	for i := 0; i < 3; i++ {
+		stepFlashes := nextStep(octopuses)
+		totalFlashes += stepFlashes
+	}
+
+	return totalFlashes, nil
+}
+
+func nextStep(octopuses [][]int) int {
+	for x := 0; x < len(octopuses); x++ {
+		for y := 0; y < len(octopuses[x]); y++ {
+			octopuses[x][y]++
+		}
+	}
+
+	for x := 0; x < len(octopuses); x++ {
+		for y := 0; y < len(octopuses[x]); y++ {
+			if energized(x, y, octopuses) {
+				flash(x, y, octopuses)
+			}
+		}
+	}
+
+	flashes := 0
+	for x := 0; x < len(octopuses); x++ {
+		for y := 0; y < len(octopuses[x]); y++ {
+			if octopuses[x][y] == 0 {
+				flashes++
+			}
+			fmt.Printf("%d ", octopuses[x][y])
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+	return flashes
+}
+
+func flash(x, y int, octopuses [][]int) {
+	octopuses[x][y] = 0
+
+	for dx := -1; dx <= 1; dx++ {
+		for dy := -1; dy <= 1; dy++ {
+			if dx == 0 && dy == 0 {
+				continue
+			}
+
+			adjX := x + dx
+			adjY := y + dy
+			hasAdjacent := adjX >= 0 && adjX < len(octopuses) && adjY >= 0 && adjY < len(octopuses[x])
+			if !hasAdjacent {
+				continue
+			}
+
+			octopuses[adjX][adjY]++
+			if energized(adjX, adjY, octopuses) {
+				flash(adjX, adjY, octopuses)
+			}
+		}
+	}
+}
+
+func energized(i, j int, octopuses [][]int) bool {
+	return octopuses[i][j] > 9
 }
 
 func loadOctopusGrid(filepath string) ([][]int, error) {
